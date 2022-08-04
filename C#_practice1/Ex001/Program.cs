09276da6f6a901928e6705793fe62a496aac5d6d
Linika <lidya-novikova.95@yandex.ru>
2022-08-05 00:11:42 +0300

    Ex001

---
+++ C#_practice1/Ex001/Program.cs
@@ -0,0 +1,9 @@
+﻿System.Console.WriteLine("Введите первое число:");
+short a=Convert.ToInt16(System.Console.ReadLine());
+System.Console.WriteLine("Введите второе число:");
+short b=Convert.ToInt16(System.Console.ReadLine());
+
+if (a>b) 
+System.Console.WriteLine("Максимальное число: " + a + "\nМинимальное: "+ b);
+else 
+System.Console.WriteLine("Максимальное число: " + b + "\nМинимальное: "+ a);
