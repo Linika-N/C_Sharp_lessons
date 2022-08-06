f90bcf8ce7dc410db2e24cc5ee8f8ce2096eada4
Linika <lidya-novikova.95@yandex.ru>
2022-08-06 13:55:24 +0300

    Ex003

---
+++ C#_practice1/Ex003/Program.cs
@@ -0,0 +1,10 @@
+﻿//Определение четного числа
+
+System.Console.WriteLine("Введите число: ");
+short a=Convert.ToInt16(System.Console.ReadLine());
+if (a%2==0){
+    System.Console.WriteLine("да");
+}
+else{
+    System.Console.WriteLine("нет");
+}
