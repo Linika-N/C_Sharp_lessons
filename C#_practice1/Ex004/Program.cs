d4c602be41115ef258712a5f3ab77019ba8d79f1
Linika <lidya-novikova.95@yandex.ru>
2022-08-06 14:09:44 +0300

    Ex004

---
+++ C#_practice1/Ex004/Program.cs
@@ -0,0 +1,13 @@
+﻿//Найти все четные числа от 1 до N
+
+System.Console.WriteLine("Введите число: ");
+byte n=Convert.ToByte(Console.ReadLine());
+int count=1;
+string result="";
+while (count<=n){
+    if (count%2==0){
+        result+=count.ToString()+ " ";
+    }
+    count++;
+}
+System.Console.WriteLine(result);
