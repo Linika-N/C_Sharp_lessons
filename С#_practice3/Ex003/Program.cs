97cfcda9c0055d6421456319c8b12367ada9847e
Linika <lidya-novikova.95@yandex.ru>
2022-08-14 13:41:15 +0300

    Ex003

---
+++ С#_practice3/Ex003/Program.cs
@@ -0,0 +1,23 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+
+int Prompt(string message){     //Ввод пятизначного числа
+    System.Console.WriteLine(message);
+    string value = Console.ReadLine();
+    int result = int.Parse(value);
+    return result;
+}
+
+void CubeNumbers(int n){
+    int result = 1;
+    for (int count = 1; count <= n; count++){
+        result = Convert.ToInt32(Math.Pow(count,3));
+        System.Console.Write(result + " ");
+    }
+}
+
+
+int n = Prompt("Введите число: ");
+CubeNumbers(n);
