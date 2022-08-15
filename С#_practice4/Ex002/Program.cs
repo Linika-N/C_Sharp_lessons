4467ca58b64abd573bd0f2018dd7b1fd51a3f8db
Linika <lidya-novikova.95@yandex.ru>
2022-08-15 23:47:39 +0300

    Ex002

---
+++ С#_practice4/Ex002/Program.cs
@@ -0,0 +1,26 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+int Prompt(string message){     //Ввод пятизначного числа
+    System.Console.WriteLine(message);
+    string value = Console.ReadLine();
+    int result = int.Parse(value);
+    return result;
+}
+
+int SumNumbers(int number){
+    int sum = 0;
+    if(number <1) 
+    number*=-1;
+    while (number >1){
+        sum+= number%10;
+        number /=10;
+    }
+    return sum;
+}
+
+int number = Prompt("Введите число");
+int answer = SumNumbers(number);
+Console.WriteLine(answer);
