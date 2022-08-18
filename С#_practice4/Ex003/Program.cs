3d18a35ec39a251d6855c0b4445a9dc06a9a34e2
Linika <lidya-novikova.95@yandex.ru>
2022-08-18 22:32:22 +0300

    Modified Ex003 - now is better working

--- С#_practice4/Ex003/Program.cs
+++ С#_practice4/Ex003/Program.cs
@@ -8,16 +8,17 @@ int[] ArrayFill(){     //Ввод массива чисел
     System.Console.WriteLine("Массив чисел: ");
     int[] arr1 = new int [8]; 
     for (int i= 0; i <arr1.Length; i++){
-        arr1[i] = new Random().Next(1,50);
+        arr1[i] = new Random().Next(7,9);
     }
     return arr1;
 }
+
 
 int FindSecondMax(int[] array){
     int max1=array[0];
     int max2= array[1];
     for (int i=0; i< array.Length; i++){
-        if(array[i]>max1){
+        if(array[i]>=max1){
             max2 = max1;
             max1 = array[i];
         }
