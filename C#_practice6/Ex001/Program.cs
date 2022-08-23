52ad4ca002bb0ceb0fb30b50ebf2d63b13828328
Linika <lidya-novikova.95@yandex.ru>
2022-08-23 22:43:35 +0300

    Ex001

---
+++ C#_practice6/Ex001/Program.cs
@@ -0,0 +1,31 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 4
+
+
+//Ход программы
+string stringNumbers = Prompt("Введите числа через пробел");
+int result = CountNumbers(stringNumbers);
+System.Console.WriteLine($"{result} раз(а) пользователь ввел число больше 0");
+
+string Prompt(string message)
+{   //Ввод строки чисел через пробел
+    System.Console.WriteLine(message);
+    return Console.ReadLine();
+}
+
+int CountNumbers(string str)
+{   // Счетчик положительных чисел
+    int count = 0;
+    string[] numStrs = str.Trim().Split(" ");
+    for (int i = 0; i < numStrs.Length; i++)
+    {
+        bool isNum = int.TryParse(numStrs[i], out int nullNum);
+        if (isNum)
+        {
+            int number = int.Parse(numStrs[i]);
+            if (number > 0) count++;
+        }
+    }
+    return count;
+}
